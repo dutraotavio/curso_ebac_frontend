7d4dc6cfb9c3441aa2485dae64cec2a1fc1c3871
Otávio Dutra <43827669+dutraotavio@users.noreply.github.com>
2024-06-20 17:39:27 -0300

    Add files via upload
    Atividade JS

---
+++ main.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form-compare')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+        
+    const valorA = document.getElementById('valor-a').value
+    const valorB = document.getElementById('valor-b').value
+
+    const mensagemSucesso = `SUCESSO! O valor B é maior que o valor A!`
+    const mensagemErro = `ERRO! O valor b é menor ou igual ao valor A!`
+
+    valorB <= valorA ? alert(mensagemErro) : alert(mensagemSucesso) 
+})
